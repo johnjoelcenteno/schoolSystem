7d8efafa276ba54bee1482e2257081e69be5c1cb
joejoe123312 <joelcenteno123312@gmail.com>
2020-09-29 22:46:42 +0800

    added login page and dashboard

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -4,13 +4,38 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Login extends CI_Controller
 {
 
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Main_model');
+	}
+
 	public function index()
 	{
+		//notifications
+		$this->Main_model->alertPromt('Invalid user', 'userInvalid');
+
 		$this->form_validation->set_rules('username', 'Username', 'required');
 		$this->form_validation->set_rules('password', 'Password', 'required');
 		if ($this->form_validation->run()) {
 			$username = $this->input->post('username');
 			$password = $this->input->post('password');
+
+			//validate username and password
+			$where['username'] = $username;
+			$where['password'] = $this->Main_model->passwordEncryptor($password);
+			$credentialsTable = $this->Main_model->multiple_where('credentials', $where);
+
+			if (count($credentialsTable->result_array()) != 0) {
+				//meron siyang nahanap
+
+				//validate and take account the user type
+				$credentialsTable = $credentialsTable->row();
+			} else {
+				//wala siyang nahanap
+				$this->session->set_userdata('userInvalid', 1);
+				redirect('Login');
+			}
 		}
 
 		$this->load->view('login');
--- application/views/login.php
+++ application/views/login.php
@@ -3,10 +3,10 @@
 <form action="" method="post">
     <?= validation_errors("<p>") ?>
     <label for="">Username</label><br>
-    <input type="text" name="username" placeholder="Enter username"><br><br>
+    <input type="text" name="username" placeholder="Enter username" autocomplete="off"><br><br>
 
     <label for="">Username</label><br>
-    <input type="password" name="password" placeholder="Enter password"><br><br>
+    <input type="password" name="password" placeholder="Enter password" autocomplete="off"><br><br>
 
     <button type="submit">Login</button>
 </form>
