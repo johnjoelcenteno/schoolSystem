c55760a36f2091a618fddc332677c311744d6d1a
jhayramirez <jhayramirez014@gmail.com>
2021-09-13 14:21:36 +0800

    404 page modified and form post modified

---
+++ application/controllers/MyCustom404Ctrl.php
@@ -0,0 +1,17 @@
+<?php
+// Inside application/controllers/MyCustom404Ctrl.php
+class MyCustom404Ctrl extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $this->output->set_status_header('404');
+
+        // Make sure you actually have some view file named 404.php
+        $this->load->view('components/404');
+    }
+}
