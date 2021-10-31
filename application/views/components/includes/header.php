5ef5ed7858f5e1b3b6133637b7942df1a88d468e
jhayramirez <jhayramirez014@gmail.com>
2021-10-31 15:41:33 +0800

    sms

--- application/views/components/includes/header.php
+++ application/views/components/includes/header.php
@@ -142,7 +142,6 @@
                 <!-- DOC: Set data-keep-expand="true" to keep the submenues expanded -->
                 <!-- DOC: Set data-auto-speed="200" to adjust the sub menu slide up/down speed -->
                 <ul class="page-sidebar-menu " data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200">
-
                     <li class="start active ">
                         <a href=<?= base_url() . "Dashboard" ?>>
                             <i class="icon-home"></i>
